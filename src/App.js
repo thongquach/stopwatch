9498eabbe1a82ffffd287dc71a3f723ef510b144
Phat Tran <phat.tran@employmenthero.com>
2022-04-12 18:40:51 +0700

    initialize code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,131 @@
 import logo from "./logo.svg";
+import { useEffect, useState } from "react";
 import "./App.css";
+function UseNow() {
+  const [now, setNow] = useState(Date.now());
 
-function App() {
+  useEffect(() => {
+    let id;
+    function repaint() {
+      setNow(Date.now());
+      id = requestAnimationFrame(repaint);
+    }
+    repaint();
+    return () => {
+      cancelAnimationFrame(id);
+    };
+  }, []);
+  return now;
+}
+
+function Duration({ durationMs }) {
+  const hh = String(Math.floor(durationMs / 1000 / 60 / 60)).padStart(2, "0");
+  const mm = String(Math.floor(durationMs / 1000 / 60) % 60).padStart(2, "0");
+  const ss = String(Math.floor(durationMs / 1000) % 60).padStart(2, "0");
+  const ms = String(durationMs / 1000).padStart(3, "0");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React s
-        </a>
-      </header>
+    <div>
+      <code>
+        <span>{hh}</span>:<span>{mm}</span>:<span>{ss}</span>.<span>{ms}</span>
+      </code>
+    </div>
+  );
+}
+
+function StopWatchinInitial({ requestStart }) {
+  const durationMs = 0;
+  return (
+    <div>
+      <h4>Initial</h4>
+      <Duration durationMs={durationMs} />
+      <footer>
+        <button onClick={requestStart}>Start</button>
+      </footer>
+    </div>
+  );
+}
+
+function StopWatchRunning({ requestPause, requestStop, startTimeMs }) {
+  const durationMs = UseNow() - startTimeMs;
+  return (
+    <div>
+      <h4>Running</h4>
+      <Duration durationMs={durationMs} />
+      <footer>
+        <button onClick={requestPause}>Pause</button>
+        <button onClick={requestStop}>Stop</button>
+      </footer>
+    </div>
+  );
+}
+
+function StopWatchPausing({ requestResume, requestReset, startTimeMs }) {
+  return (
+    <div>
+      <h4>Pausing</h4>
+      <Duration durationMs={startTimeMs} />
+      <footer>
+        <button onClick={requestResume}>Resume</button>
+        <button onClick={requestReset}>Stop</button>
+      </footer>
+    </div>
+  );
+}
+function App() {
+  const [isActive, setIsActive] = useState(false);
+  const [isPaused, setIsPaused] = useState(true);
+  const [time, setTime] = useState(Date.now());
+
+  const initial = !isActive;
+  const running = isActive && !isPaused;
+  const isPausing = isActive && isPaused;
+
+  const handleStart = () => {
+    setIsActive(true);
+    setIsPaused(false);
+  };
+
+  const handlePauseResume = () => {
+    setIsPaused(!isPaused);
+    const durationMs = time;
+    setTime(durationMs);
+  };
+
+  const handleReset = () => {
+    setIsActive(false);
+    setTime(Date.now());
+  };
+  return (
+    <div>
+      {initial && (
+        <div>{<StopWatchinInitial requestStart={handleStart} />}</div>
+      )}
+
+      {running && (
+        <div>
+          {
+            <StopWatchRunning
+              requestPause={handlePauseResume}
+              requestStop={handleReset}
+              startTimeMs={time}
+              setTime={setTime}
+            />
+          }
+        </div>
+      )}
+
+      {isPausing && (
+        <div>
+          {
+            <StopWatchPausing
+              requestPause={handlePauseResume}
+              requestStop={handleReset}
+              startTimeMs={time}
+            />
+          }
+        </div>
+      )}
     </div>
   );
 }
